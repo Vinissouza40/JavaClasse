8be7be5c8772c69c34f93985e5aa7e667e404bcb
Vinicius Souza <viniciussantos.souza40@gmail.com>
2022-09-09 17:07:30 -0300

    Modificacao

--- DataTeste.java
+++ DataTeste.java
@@ -4,14 +4,9 @@ public class DataTeste {
 	public static void main(String[] args) {
 		
 		Data d1 = new Data();
-		d1.dia = 7;
-		d1.mes = 11;
-		d1.ano = 2021;
 		
-		Data d2 = new Data();
-		d2.dia = 31;
-		d2.mes = 12;
-		d2.ano = 2020;
+		Data d2 = new Data(31,12,2020);
+		
 		
 		
 		System.out.println(d1.obterDataFormatada());
--- ProdutoTeste.java
+++ ProdutoTeste.java
@@ -4,13 +4,12 @@ package classe;
 public class ProdutoTeste {
 	public static void main(String[] args) {
 		
-		Produto p1 = new Produto();
-		p1.nome = "Notebook";
+		Produto p1 = new Produto("Notebook", 4356.89, 0.25);
 		p1.preco = 4356.89;
 		p1.desconto = 0.25;
 		
-        Produto p2 = new Produto();
-        p2.nome = "Caneta Preta";
+        Produto p2 = new Produto("Caneta Preta");
+        p2.nome = "Caneta Preta2";
         p2.preco = 12.56;
         p2.desconto = 0.29;
         
