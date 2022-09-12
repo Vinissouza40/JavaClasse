38dc6d0cb59b2a75ca0df242fa7055e5a9c36cb0
Vinicius Souza <viniciussantos.souza40@gmail.com>
2022-09-12 08:34:19 -0300

    Mod

--- DataTeste.java
+++ DataTeste.java
@@ -7,9 +7,13 @@ public class DataTeste {
 		
 		Data d2 = new Data(31,12,2020);
 		
+		String dataFormatada1 = d1.obterDataFormatada();
 		
-		
-		System.out.println(d1.obterDataFormatada());
+		System.out.println(dataFormatada1);
 		System.out.printf(d2.obterDataFormatada());
+		
+		
+		d1.imprimirDataFormatada();
+		d2.imprimirDataFormatada();
 	}
 }
